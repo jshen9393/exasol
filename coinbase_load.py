e59d11622b543fb7e25b9aff3af1c571b7e1e4ea
Shen <jimmyshen@RTI-107997.rti.ns>
2021-02-28 20:31:04 -0500

    updating coinbase logic

---
+++ coinbase_load.py
@@ -0,0 +1,6 @@
+#
+# Use this API link https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
+# Create a arg to pass in arguments to indicate if its a one time load or a daily load
+# Create a function that writes in a 6 month load using dynamic days. use bulk load to load all the data
+# Create a function thad loads the data on a daily level.
+#
