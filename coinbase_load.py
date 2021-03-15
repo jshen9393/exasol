9c519e9b092f81772083d06f25aeb93ceab0762b
jimmyshen <js@jimmyshen.com>
2021-03-15 08:39:06 -0500

    Saving coinbase load

--- coinbase_load.py
+++ coinbase_load.py
@@ -4,3 +4,18 @@
 # Create a function that writes in a 6 month load using dynamic days. use bulk load to load all the data
 # Create a function thad loads the data on a daily level.
 #
+
+import requests
+from datetime import date
+from datetime import timedelta
+import json
+
+today = date.today()
+six_months = today - timedelta(days=180)
+url = f"https://api.coindesk.com/v1/bpi/historical/close.json?start={six_months}&end={today}"
+r = requests.get(url)
+results = json.loads(r.text)
+print(results["bpi"])
+
+
+
